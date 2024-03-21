bd59a4557015a7986579d2693918c81e9abfed2b
ponpon <ponponumi@gmail.com>
2024-03-21 16:07:24 +0900

    テスト用のファイルを作成

---
+++ test/index.php
@@ -0,0 +1,11 @@
+<?php require __DIR__ . "/header.php" ?>
+
+<?php
+
+$text = "メールアドレス hoge@example.com hogehoge@example.com";
+
+?>
+
+<p><?= $text ?></p>
+
+<?php require __DIR__ . "/footer.php" ?>
