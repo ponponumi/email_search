467720ac473397ec1b59341da4884887c46acf06
ponpon <ponponumi@gmail.com>
2024-03-21 16:24:13 +0900

    posを返すメソッドのテストを作成

--- test/index.php
+++ test/index.php
@@ -5,11 +5,13 @@
 $text = "メールアドレス hoge@example.com hogehoge@example.com hoge-12345.hoge_hoge@example.com";
 
 $list = \Ponponumi\EmailSearch\EmailSearch::search($text);
+$list_pos = \Ponponumi\EmailSearch\EmailSearch::searchPos($text);
 
 ?>
 
 <p><?= $text ?></p>
 
 <pre><?php var_dump($list) ?></pre>
+<pre><?php var_dump($list_pos) ?></pre>
 
 <?php require __DIR__ . "/footer.php" ?>
