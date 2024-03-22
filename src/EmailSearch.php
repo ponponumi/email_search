5d1329554a2c64a76440142ca766448b972a508b
ponpon <ponponumi@gmail.com>
2024-03-23 01:43:11 +0900

    パターンを取得するメソッドを使うように変更

--- src/EmailSearch.php
+++ src/EmailSearch.php
@@ -9,7 +9,8 @@ class EmailSearch{
 
   public static function search(string $text){
     // メールアドレスを抽出する
-    preg_match_all('/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/', $text, $match_list);
+    $pattern = self::patternGet();
+    preg_match_all($pattern, $text, $match_list);
     $email_list = $match_list[0];
     return $email_list;
   }
